8cf23d8e06c12ed93fa69e0d19ec773e29058340
Shridhar202 <shridhargawali2002@gmail.com>
2023-05-20 22:28:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Add from './components/Add/Add';
+import Sub from './components/Sub/Sub';
+
+function App() {
+  return (
+      <div>
+        <h1> First React Project </h1>
+        <p> test p tag </p>
+
+        <Add a = '2' b = '2'> </Add>
+        <Sub a = '2' b = '2'/>
+
+      </div>    
+  );
+}
+
+export default App;
---
+++ src/components/Sub/Sub.js
@@ -0,0 +1,9 @@
+const Sub = () => {
+    return (
+        <div>
+            <p> This is Sub component </p>
+        </div>
+    );
+}
+
+export default Sub;
